41e65641c808319e281bccc3640d711a00c98730
bmackenzie <39527613+bmackenzie@users.noreply.github.com>
2022-08-26 14:47:59 -0500

    added dtyped to databases

--- app.py
+++ app.py
@@ -28,25 +28,28 @@ def generate_table(dataframe, max_rows=5):
 with sqlite3.connect('mlsdb.sqlite') as conn:
     query = conn.execute("""
     SELECT
-    p.Player Name,
+    p.name Name,
     p.squad Squad,
     ps.gls Goals
     FROM playerShooting ps
     INNER JOIN players p
-    	ON ps.Id = p.Id
+    	ON ps.player_id = p.id
     ORDER BY goals DESC;""")
     cols = [column[0] for column in query.description]
     goalLeaders= pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+
+
+goalFig = px.bar(goalLeaders.sort_values('Goals', ascending = False).head(20), x='Name', y="Goals", color = 'Squad', template = 'plotly', title = 'Top Goal Scorers')
 #Assist Leaders
 with sqlite3.connect('mlsdb.sqlite') as conn:
     query = conn.execute("""
     SELECT
-    p.Player Name,
+    p.name Name,
     p.squad Squad,
     pp.ast Assists
     FROM playerPassing pp
     INNER JOIN players p
-    	ON pp.Id = p.Id
+    	ON pp.player_id = p.id
     ORDER BY Assists DESC;""")
     cols = [column[0] for column in query.description]
     astLeaders= pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
@@ -54,12 +57,12 @@ with sqlite3.connect('mlsdb.sqlite') as conn:
 with sqlite3.connect('mlsdb.sqlite') as conn:
     query = conn.execute("""
     SELECT
-    p.Player Name,
+    p.name Name,
     p.squad Squad,
     pg.scPassLive + pg.scPassDead "Shot Creating Passes"
     FROM playerGsc pg
     INNER JOIN players p
-    	ON pg.Id = p.Id
+    	ON pg.player_id = p.id
     ORDER BY "Shot Creating Passes" DESC;""")
     cols = [column[0] for column in query.description]
     scLeaders= pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
@@ -67,12 +70,12 @@ with sqlite3.connect('mlsdb.sqlite') as conn:
 with sqlite3.connect('mlsdb.sqlite') as conn:
     query = conn.execute("""
     SELECT
-    p.Player Name,
+    p.name Name,
     p.squad Squad,
     d.tklW "Won Tackles"
-    FROM defense d
+    FROM playerDefense d
     INNER JOIN players p
-    	ON d.Id = p.Id
+    	ON d.player_id = p.id
     ORDER BY "Won Tackles" DESC;""")
     cols = [column[0] for column in query.description]
     tklWLeaders= pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
@@ -80,12 +83,12 @@ with sqlite3.connect('mlsdb.sqlite') as conn:
 with sqlite3.connect('mlsdb.sqlite') as conn:
     query = conn.execute("""
     SELECT
-    p.Player Name,
+    p.name Name,
     p.squad Squad,
     ROUND(d.tklW/ (d.tklAttThird + d.tklDefThird + d.tklMidThird), 2) "Tackle Percentage"
-    FROM defense d
+    FROM playerDefense d
     INNER JOIN players p
-    	ON d.Id = p.Id
+    	ON d.player_id = p.id
     WHERE (d.tklAttThird + d.tklDefThird + d.tklMidThird) > 25
     ORDER BY "Tackle Percentage" DESC;""")
     cols = [column[0] for column in query.description]
@@ -94,15 +97,29 @@ with sqlite3.connect('mlsdb.sqlite') as conn:
 with sqlite3.connect('mlsdb.sqlite') as conn:
     query = conn.execute("""
     SELECT
-    p.Player Name,
+    p.name Name,
     p.squad Squad,
     d.int Interceptions
-    FROM defense d
+    FROM playerDefense d
     INNER JOIN players p
-    	ON d.Id = p.Id
+    	ON d.player_id = p.id
     ORDER BY Interceptions DESC;""")
     cols = [column[0] for column in query.description]
     tklPercentLeaders= pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+#Highest Save Percentage among keepers with at least 10 games
+with sqlite3.connect('mlsdb.sqlite') as conn:
+    query = conn.execute("""
+    SELECT
+    p.name Name,
+    p.squad Squad,
+    ROUND(CAST(k.saves AS float)/CAST(k.sota AS float), 2) "Save Percentage"
+    FROM playerKeepers k
+    INNER JOIN players p
+    	ON k.player_id = p.id
+    WHERE k.starts > 10
+    ORDER BY "Save Percentage" DESC;""")
+    cols = [column[0] for column in query.description]
+    savePercentLeaders= pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
 
 
 app.layout = html.Div(children=[
@@ -111,6 +128,11 @@ app.layout = html.Div(children=[
 
     html.H4(children='Top Goal Scorers'),
     generate_table(goalLeaders),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=goalFig
+    ),
 
     html.H4(children='Most Assists'),
     generate_table(astLeaders),
@@ -122,7 +144,10 @@ app.layout = html.Div(children=[
     generate_table(tklWLeaders),
 
     html.H4(children='Highest Tackle Percent (Minimum 25 tackles)'),
-    generate_table(tklPercentLeaders)
+    generate_table(tklPercentLeaders),
+
+    html.H4(children='Highest Save Percent (Minimum 10 starts)'),
+    generate_table(savePercentLeaders)
 ])
 
 if __name__ == '__main__':
